9bafc6b2ccf143e54bde05ef3c12fd1699a876ac
js-easy-school <supergeroy11@gmail.com>
2023-08-11 23:51:19 +0300

    modified:   src/app/api/fake.api/professions.api.js 	modified:   src/app/api/fake.api/user.api.js 	modified:   src/app/components/common/form/textField.jsx

--- src/app/components/common/form/textField.jsx
+++ src/app/components/common/form/textField.jsx
@@ -1,6 +1,5 @@
-// Импортируем библиотеки React и PropTypes
-import React, { useState } from "react"
-import PropTypes from "prop-types"
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 // Определяем компонент TextField, принимающий несколько пропсов
 const TextField = ({ label, type, name, value, onChange, error }) => {
@@ -15,15 +14,15 @@ const TextField = ({ label, type, name, value, onChange, error }) => {
             <div className='input-group col-sm-0 has-validation'>
                 <input
                     className={`border-2 form-control${
-                        error ? " is-invalid" : ""
+                        error ? ' is-invalid' : ''
                     }`}
-                    type={showPassword ? "text" : type}
+                    type={showPassword ? 'text' : type}
                     id={name}
                     name={name}
                     value={value}
                     onChange={onChange}
                 />
-                {type === "password" && (
+                {type === 'password' && (
                     <button
                         className='btn btn-outline-secondary'
                         type='button'
@@ -31,7 +30,7 @@ const TextField = ({ label, type, name, value, onChange, error }) => {
                     >
                         <i
                             className={
-                                "bi bi-eye" + (showPassword ? "-slash" : "")
+                                'bi bi-eye' + (showPassword ? '-slash' : '')
                             }
                         ></i>
                     </button>
@@ -46,7 +45,7 @@ const TextField = ({ label, type, name, value, onChange, error }) => {
 
 // Устанавливаем значение по умолчанию для пропса type, если он не передан
 TextField.defaultProps = {
-    type: "text",
+    type: 'text'
 }
 
 // Описываем, какие пропсы принимает компонент TextField и какого типа они должны быть
@@ -56,7 +55,7 @@ TextField.propTypes = {
     name: PropTypes.string,
     value: PropTypes.string,
     onChange: PropTypes.func,
-    error: PropTypes.string,
+    error: PropTypes.string
 }
 
 // Экспортируем компонент TextField для его использования в других частях приложения

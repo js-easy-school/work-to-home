bd96e49430a1d9579aefee795ff8c3cbbc37deea
js-easy-school <supergeroy11@gmail.com>
2023-08-13 17:17:46 +0300

    modified:   .prettierrc 	new file:   project-lesson-module-9-task-2/my-login-form/src/.prettierrc 	modified:   project-lesson-module-9-task-2/my-login-form/src/App.js 	modified:   project-lesson-module-9-task-2/my-login-form/src/App2.js 	modified:   project-lesson-module-9-task-2/my-login-form/src/components/studentCard.jsx 	modified:   project-lesson-module-9-task-2/my-login-form/src/components/studentForm.jsx 	modified:   project-lesson-module-9-task-2/my-login-form/src/index.js 	modified:   src/app/components/detailsPage.jsx 	modified:   src/app/components/formUlbi.jsx

--- project-lesson-module-9-task-2/my-login-form/src/components/studentCard.jsx
+++ project-lesson-module-9-task-2/my-login-form/src/components/studentCard.jsx
@@ -3,12 +3,10 @@ function StudentCard({ student, onEdit }) {
     const age = new Date().getFullYear() - student.birthYear
     const yearsWord =
         age % 10 === 1 && age % 100 !== 11
-            ? 'год'
-            : age % 10 >= 2 &&
-              age % 10 <= 4 &&
-              (age % 100 < 10 || age % 100 >= 20)
-            ? 'года'
-            : 'лет'
+            ? "год"
+            : age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)
+            ? "года"
+            : "лет"
 
     return (
         <div>
--- src/app/components/formUlbi.jsx
+++ src/app/components/formUlbi.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react"
 
 const FormUlbi = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const [emailDirty, setEmailDirty] = useState(false)
     const [passwordDirty, setPasswordDirty] = useState(false)
-    const [emailError, setEmailError] = useState('Емейл не может быть пустым')
-    const [passwordError, setPasswordError] = useState(
-        'Пароль не может быть пустым'
-    )
+    const [emailError, setEmailError] = useState("Емейл не может быть пустым")
+    const [passwordError, setPasswordError] = useState("Пароль не может быть пустым")
     const [formValid, setFormValid] = useState(false)
 
     useEffect(() => {
@@ -24,31 +22,31 @@ const FormUlbi = () => {
         const filter =
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         if (!filter.test(String(e.target.value).toLowerCase())) {
-            setEmailError('Некорректный емейл')
+            setEmailError("Некорректный емейл")
         } else {
-            setEmailError('')
+            setEmailError("")
         }
     }
 
     const passwordHandler = (e) => {
         setPassword(e.target.value)
         if (e.target.value.length < 3 || e.target.value.length > 8) {
-            setPasswordError('Пароль должен быть длинее 3 и меньше 8')
+            setPasswordError("Пароль должен быть длинее 3 и меньше 8")
             if (!e.target.value) {
-                setPasswordError('Пароль не может быть пустым')
+                setPasswordError("Пароль не может быть пустым")
             }
         } else {
-            setPasswordError('')
+            setPasswordError("")
         }
     }
 
     const blurHandler = (e) => {
         switch (e.target.name) {
-            case 'email':
+            case "email":
                 console.log(e.target.name)
                 setEmailDirty(true)
                 break
-            case 'password':
+            case "password":
                 setPasswordDirty(true)
                 break
             default:
@@ -60,9 +58,7 @@ const FormUlbi = () => {
         <form>
             <div className="mb-3">
                 <h1>Регистрация</h1>
-                {emailDirty && emailError && (
-                    <div style={{ color: 'red' }}>{emailError}</div>
-                )}
+                {emailDirty && emailError && <div style={{ color: "red" }}>{emailError}</div>}
                 <div>
                     <input
                         className="form-control"
@@ -74,9 +70,7 @@ const FormUlbi = () => {
                         placeholder="Enter your email..."
                     />
                 </div>
-                {passwordError && passwordDirty && (
-                    <div style={{ color: 'red' }}>{passwordError}</div>
-                )}
+                {passwordError && passwordDirty && <div style={{ color: "red" }}>{passwordError}</div>}
                 <div>
                     <input
                         className="form-control"
@@ -89,11 +83,7 @@ const FormUlbi = () => {
                     />
                 </div>
             </div>
-            <button
-                disabled={!formValid}
-                type="submit"
-                className="btn btn-primary"
-            >
+            <button disabled={!formValid} type="submit" className="btn btn-primary">
                 Registration
             </button>
         </form>
